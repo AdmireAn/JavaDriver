473c88265e4c86ffb82a2e4cb8935b3f0704708d
JavaGod <854818303@qq.com>
2022-05-20 10:05:08 +0800

    commit
    commit

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -19,7 +19,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       description: 'Java学习与总结',
     }
   },
-  // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+   base: '/JavaDriver/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
 
   // 主题配置
   themeConfig: {
