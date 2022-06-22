8c069d6b1f0b22751d82b8175a7ecc0398a87010
admirean <854818303@qq.com>
2022-06-22 22:04:32 +0800

    删除部分章节

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -59,24 +59,12 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         link: '/middleware/',
         items: [
           { text: 'Netty', link: '/pages/40f3c9/' },
-          { text: 'Spring', link: '/pages/434235/' },
-          { text: 'Mybatis', link: '/pages/3793a5/' },
           { text: 'Kafka', link: '/pages/561f34/' },
-          { text: 'Dubbo', link: '/pages/78517c/' },
         ],
       },
       { text: '系统设计', link: '/pages/01b9b2/' },
       { text: '非技术', link: '/pages/5e507a/' },
       { text: '关于', link: '/about/' },
-      {
-        text: '收藏',
-        link: '/pages/beb6c0bd8a66cea6/',
-        // items: [
-        //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-        //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-        //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
-        // ],
-      },
       {
         text: '索引',
         link: '/archives/',
